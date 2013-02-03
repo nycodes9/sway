e72b7a766edcc200b785141262ec78d5f1bd685f
raj <nycodes9@gmail.com>
2013-02-02 20:56:36 -0500

    dataobjecct commit

--- Sway/src/com/sway/app/datamodels/WishMakerObject.java
+++ Sway/src/com/sway/app/datamodels/WishMakerObject.java
@@ -2,15 +2,59 @@ package com.sway.app.datamodels;
 
 import java.util.ArrayList;
 
-import android.location.Location;
-import android.provider.ContactsContract.CommonDataKinds.Email;
+import com.stackmob.sdk.api.StackMobGeoPoint;
+import com.stackmob.sdk.model.StackMobModel;
 
-public class WishMakerObject {
+public class WishMakerObject extends StackMobModel {
 
-	long personID;
+	public WishMakerObject() {
+		super(WishMakerObject.class);
+	}
+
+	String personID;
 	String personName;
 	ArrayList<WishObject> wishListObjectArray;
-	Location lastKnownLocation;
-	Email personEmail;
-	
+	StackMobGeoPoint lastKnownLocation;
+	String personEmail;
+
+	public String getPersonID() {
+		return personID;
+	}
+
+	public void setPersonID(String personID) {
+		this.personID = personID;
+	}
+
+	public String getPersonName() {
+		return personName;
+	}
+
+	public void setPersonName(String personName) {
+		this.personName = personName;
+	}
+
+	public ArrayList<WishObject> getWishListObjectArray() {
+		return wishListObjectArray;
+	}
+
+	public void setWishListObjectArray(ArrayList<WishObject> wishListObjectArray) {
+		this.wishListObjectArray = wishListObjectArray;
+	}
+
+	public StackMobGeoPoint getLastKnownLocation() {
+		return lastKnownLocation;
+	}
+
+	public void setLastKnownLocation(StackMobGeoPoint lastKnownLocation) {
+		this.lastKnownLocation = lastKnownLocation;
+	}
+
+	public String getPersonEmail() {
+		return personEmail;
+	}
+
+	public void setPersonEmail(String personEmail) {
+		this.personEmail = personEmail;
+	}
+
 }
--- Sway/src/com/sway/app/datamodels/WishObject.java
+++ Sway/src/com/sway/app/datamodels/WishObject.java
@@ -3,17 +3,78 @@ package com.sway.app.datamodels;
 import java.net.URL;
 import java.util.Date;
 
-public class WishObject {
+import com.stackmob.sdk.model.StackMobModel;
 
-	long wishID;
+public class WishObject extends StackMobModel{
+
+	public WishObject() {
+		super(WishObject.class);
+	}
+	
+	String wishID;
 	String productName;
 	URL productURL;
+	URL productImageURL;
+	
+	String merchantID;
 	String merchantName;
 	double price;
-	
-	long merchantID;
 	
 	Date whenCreated;
 	Date whenExpires;
 	
+	public String getWishID() {
+		return wishID;
+	}
+	public void setWishID(String wishID) {
+		this.wishID = wishID;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public URL getProductURL() {
+		return productURL;
+	}
+	public void setProductURL(URL productURL) {
+		this.productURL = productURL;
+	}
+	public String getMerchantName() {
+		return merchantName;
+	}
+	public void setMerchantName(String merchantName) {
+		this.merchantName = merchantName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getMerchantID() {
+		return merchantID;
+	}
+	public void setMerchantID(String merchantID) {
+		this.merchantID = merchantID;
+	}
+	public Date getWhenCreated() {
+		return whenCreated;
+	}
+	public void setWhenCreated(Date whenCreated) {
+		this.whenCreated = whenCreated;
+	}
+	public Date getWhenExpires() {
+		return whenExpires;
+	}
+	public void setWhenExpires(Date whenExpires) {
+		this.whenExpires = whenExpires;
+	}
+	public URL getProductImageURL() {
+		return productImageURL;
+	}
+	public void setProductImageURL(URL productImageURL) {
+		this.productImageURL = productImageURL;
+	}
 }
